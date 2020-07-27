c0215b3291f7827ff0a326909109cd2bc57f6cf4
gp-add11 <iitkgp.girish@gmail.com>
2020-07-28 00:30:41 +0530

    App works  with images when rolled

--- app/src/main/java/com/gpadd11/diceroller/MainActivity.kt
+++ app/src/main/java/com/gpadd11/diceroller/MainActivity.kt
@@ -3,6 +3,7 @@ package com.gpadd11.diceroller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
 
@@ -14,12 +15,6 @@ class MainActivity : AppCompatActivity() {
 
         val rollButton: Button = findViewById(R.id.button)
         rollButton.setOnClickListener {
-            /*
-            val toast = Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT)
-            toast.show()
-            val resultTextView: TextView = findViewById(R.id.textView)
-            resultTextView.text = "6"
-             */
             rollDice()
         }
     }
@@ -27,8 +22,19 @@ class MainActivity : AppCompatActivity() {
     private fun rollDice() {
         val dice = Dice(6)
         val diceRoll = dice.roll()
-        val resultTextView: TextView = findViewById(R.id.textView)
-        resultTextView.text = diceRoll.toString()
+
+        val diceImage : ImageView = findViewById(R.id.imageView)
+
+        when(diceRoll) {
+            1 -> diceImage.setImageResource(R.drawable.dice_1)
+            2 -> diceImage.setImageResource(R.drawable.dice_2)
+            3 -> diceImage.setImageResource(R.drawable.dice_3)
+            4 -> diceImage.setImageResource(R.drawable.dice_4)
+            5 -> diceImage.setImageResource(R.drawable.dice_5)
+            else -> diceImage.setImageResource(R.drawable.dice_6)
+        }
+
+        diceImage.contentDescription = diceRoll.toString()
     }
 }
 
